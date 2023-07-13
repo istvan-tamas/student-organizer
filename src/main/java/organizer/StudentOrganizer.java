dd1e94c2f3926913c898ea37dadc10588d1af4c5
istvan-tamas <istvan.tamas90@gmail.com>
2023-07-13 11:31:12 +0200

    Reorgaizet folder structure

--- src/main/java/organizer/StudentOrganizer.java
+++ src/main/java/organizer/StudentOrganizer.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package organizer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class StudentOrganizer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(StudentOrganizer.class, args);
 	}
 
 }
