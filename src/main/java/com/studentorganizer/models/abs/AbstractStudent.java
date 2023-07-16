7693f9f4ba673181a0ebf3cfc64902f57e2a535b
istvan-tamas <istvan.tamas90@gmail.com>
2023-07-16 13:17:25 +0200

    Added basic attributes to the abstract student class

--- src/main/java/com/studentorganizer/models/abs/AbstractStudent.java
+++ src/main/java/com/studentorganizer/models/abs/AbstractStudent.java
@@ -1,5 +1,9 @@
 package com.studentorganizer.models.abs;
 
 public abstract class AbstractStudent {
-    
+    private String neptune;
+    private String firstName;
+    private String lastName;
+    private String major;
+    private String educationType;
 }
