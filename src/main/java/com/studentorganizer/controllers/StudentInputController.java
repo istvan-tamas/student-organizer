6aea5c491d18e11bc12b2f56404d67534722f511
istvan-tamas <istvan.tamas90@gmail.com>
2023-07-17 16:24:49 +0200

    log service implemented

--- src/main/java/com/studentorganizer/controllers/StudentInputController.java
+++ src/main/java/com/studentorganizer/controllers/StudentInputController.java
@@ -1,16 +1,19 @@
 package com.studentorganizer.controllers;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.studentorganizer.models.DTO.StudentDTO;
+import com.studentorganizer.services.LogService;
 
 
 @Controller
 public class StudentInputController {
+    
+    @Autowired
+    private LogService logService;
+    
     @GetMapping("/student-input")
-    public String inputForm(Model model){
-        model.addAttribute("inputparams", new StudentDTO());
-        return "student-input";
+    public void status() {
+        logService.log();
     }
 }
