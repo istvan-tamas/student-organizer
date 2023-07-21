409ae88ef21f743620ef0133d8f777d2a88a7188
istvan-tamas <istvan.tamas90@gmail.com>
2023-07-21 20:50:18 +0200

    Added StudentService which implements the StudentCRUDInterface

---
+++ src/main/java/com/studentorganizer/services/StudentService.java
@@ -0,0 +1,31 @@
+package com.studentorganizer.services;
+
+import com.studentorganizer.interfaces.StudentCRUDInterface;
+
+public class StudentService implements StudentCRUDInterface {
+
+    @Override
+    public void createStudent(String neptune, String firstName, String lastName, String major, String educationType) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createStudent'");
+    }
+
+    @Override
+    public void readStudent() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'readStudent'");
+    }
+
+    @Override
+    public void updateStudent(String neptune, String firstName, String lastName, String major, String educationType) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateStudent'");
+    }
+
+    @Override
+    public void deleteStudent() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteStudent'");
+    }
+    
+}
