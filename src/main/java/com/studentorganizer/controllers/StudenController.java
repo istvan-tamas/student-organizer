3333ba8284ac40ace3af215d8d535f019240e69f
istvan-tamas <istvan.tamas90@gmail.com>
2023-07-24 19:48:02 +0200

    Create Student field added

--- src/main/java/com/studentorganizer/controllers/StudenController.java
+++ src/main/java/com/studentorganizer/controllers/StudenController.java
@@ -5,7 +5,9 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -40,11 +42,16 @@ public class StudenController {
  //   public void addStudent(@RequestBody StudentDTO student){
  //       studentService.createStudent(student);
  //   }
-
-    @RequestMapping(method=RequestMethod.POST, value = "/students")
-    public String addStudent(Model model, @RequestBody StudentDTO student){
+    @GetMapping("/student-input")
+    public String showNewStudentform(Model model, StudentDTO student){
         model.addAttribute("student", studentService.createStudent(student));
-        return "/students";
+        return "new-student";
+    }
+
+    @PostMapping("/student-input")
+    public String addStudent(Model model, StudentDTO student){
+        model.addAttribute("student", studentService.createStudent(student));
+        return "redirect:/students";
     }
 
     @RequestMapping(method=RequestMethod.PUT, value = "/students/{id}")
