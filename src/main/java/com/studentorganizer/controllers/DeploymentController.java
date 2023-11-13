9dfb0eb462ed6267f9f2deaeae6087c4e534ded2
istvan-tamas <istvan.tamas90@gmail.com>
2023-11-13 18:48:44 +0100

    CSV resource bug ic

--- src/main/java/com/studentorganizer/controllers/DeploymentController.java
+++ src/main/java/com/studentorganizer/controllers/DeploymentController.java
@@ -2,8 +2,7 @@ package com.studentorganizer.controllers;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.InputStream;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,14 +12,13 @@ import com.studentorganizer.services.DeployService;
 
 @Controller
 public class DeploymentController {
-    
 
-public static List<List<String>> records = new ArrayList<>();
+    InputStream inputStream = getClass().getResourceAsStream("/MOCK_DATA.csv");
 
     @GetMapping("/")
     public String deploy(Model model) throws FileNotFoundException, IOException{
         try{
-            DeployService.start();
+            DeployService.start(inputStream);
         }
         catch(Exception e){
             System.out.println("Error: " + e);
--- src/main/java/com/studentorganizer/services/DeployService.java
+++ src/main/java/com/studentorganizer/services/DeployService.java
@@ -7,6 +7,9 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import org.springframework.stereotype.Service;
 
@@ -15,14 +18,27 @@ import com.studentorganizer.models.Student;
 @Service
 public class DeployService {
     public static List<Student> records = new ArrayList<>();
-    public static void start() throws FileNotFoundException, IOException {
+    // statikusan nincsen null pointer de így az erőforrást nem mindig találja meg
+    public static void start(InputStream inputStream) throws FileNotFoundException, IOException {
         String COMMA_DELIMITER = ",";
-        try(BufferedReader br = new BufferedReader(new FileReader("student-organizer/src/main/resources/csv-db/MOCK_DATA.csv"))){
-            String line;
-                while((line = br.readLine()) != null){
-                String[] values = line.split(COMMA_DELIMITER);
-                records.add(new Student(values[0], values[1], values[2],values[3],values[4],UUID.randomUUID().toString()));
-            }
+        try (inputStream)
+        {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                while((line = reader.readLine()) != null)
+                {
+                    String[] values = line.split(COMMA_DELIMITER);
+                    records.add(new Student(values[0], values[1], values[2],values[3],values[4],UUID.randomUUID().toString()));
+                }
+
         }
+        
+//        try(BufferedReader br = new BufferedReader(new FileReader("student-organizer/src/main/resources/csv-db/MOCK_DATA.csv"))){
+//            String line;
+//                while((line = br.readLine()) != null){
+//                String[] values = line.split(COMMA_DELIMITER);
+//                records.add(new Student(values[0], values[1], values[2],values[3],values[4],UUID.randomUUID().toString()));
+//            }
+//        }
     }
 }
