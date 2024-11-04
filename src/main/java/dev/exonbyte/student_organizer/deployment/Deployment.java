8c0a79a9e571452ada172300bc50f69e15910552
istvan-tamas <istvan.tamas90@gmail.com>
2024-11-04 14:31:12 +0100

    Added remote PostgreSQL access

--- src/main/java/dev/exonbyte/student_organizer/deployment/Deployment.java
+++ src/main/java/dev/exonbyte/student_organizer/deployment/Deployment.java
@@ -15,39 +15,16 @@ import java.util.List;
 @Component
 public class Deployment implements InitializingBean {
 
-//    private final StudentServiceImp studentService;
-//
-//    @Autowired
-//    public Deployment(StudentServiceImp studentService) {
-//        this.studentService = studentService;
-//    }
-//
+    private final StudentServiceImp studentService;
+
+    @Autowired
+    public Deployment(StudentServiceImp studentService) {
+        this.studentService = studentService;
+    }
+
     @Override
     public void afterPropertiesSet() throws FileNotFoundException {
-        System.out.println("Deployment");
-
-//        String fileName = System.getProperty("user.dir") + "/MOCK_DATA.csv";
-////        System.out.println(fileName);
-//
-//        List<CSV> beans = new CsvToBeanBuilder(new FileReader(fileName)).withType(CSV.class).withSkipLines(1).build().parse();
-//
-//        for (CSV bean : beans) {
-//            StudentDTO theStudent = new StudentDTO();
-//            theStudent.setId(bean.getId());
-//            theStudent.setEducationType(bean.getEducation_type());
-//            theStudent.setFirstName(bean.getFirst_name());
-//            theStudent.setLastName(bean.getLast_name());
-//            theStudent.setMajor(bean.getMajor());
-//            theStudent.setNeptune(bean.getNeptune());
-//            studentService.createStudent(theStudent);
-//        }
-//        try(PythonInterpreter pyInterp = new PythonInterpreter()){
-//            StringWriter output = new StringWriter();
-//            pyInterp.setOut(output);
-//            pyInterp.exec("print('Deployment started');')");
-//            System.out.println(output.toString());
-//            output.close();
-//        }
+        System.out.println("App started OK!");
 
     }
 
