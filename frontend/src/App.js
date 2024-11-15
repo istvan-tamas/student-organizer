8decb0af6329904fce7ca81177f70327eb850ec4
matleve98 <leventemth@gmail.com>
2024-11-15 21:30:44 +0100

    css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,22 +6,49 @@ import { StuedentList } from "./StudentList";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/student/create" style={{ marginLeft: "10px" }}>
-            Add Student
-          </Link>
-        </nav>
+    <div style={{ padding: "20px" }}>
+      <Router>
+        <div>
+          <nav>
+            <div style={{ padding: "10px" }}>
+              {/* <Link to="/">Home</Link> */}
+              <Link
+                to="/"
+                style={{
+                  marginRight: "10px",
+                  textDecoration: "none",
+                  color: "white",
+                  backgroundColor: "blue",
+                  padding: "10px",
+                  borderRadius: "5px",
+                }}
+              >
+                Home
+              </Link>
+              <Link
+                to="/student/create"
+                style={{
+                  marginLeft: "10px",
+                  textDecoration: "none",
+                  color: "white",
+                  backgroundColor: "green",
+                  padding: "10px",
+                  borderRadius: "5px",
+                }}
+              >
+                Add Student
+              </Link>
+            </div>
+          </nav>
 
-        <Routes>
-          <Route path="/" element={<StuedentList />} />
-          <Route path="/student/create" element={<AddStudent />} />
-          <Route path="/student/update/:id" element={<UpdateStudent />} />
-        </Routes>
-      </div>
-    </Router>
+          <Routes>
+            <Route path="/" element={<StuedentList />} />
+            <Route path="/student/create" element={<AddStudent />} />
+            <Route path="/student/update/:id" element={<UpdateStudent />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
   );
 }
 
